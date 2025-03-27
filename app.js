274093350a3abebb072aab27cf7949f064491078
abu0127 <sr20010127@gmail.com>
2025-03-27 22:44:30 +0300

    w

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function main() {
         let scrollpos = window.scrollY;
 
         sections.forEach((section, index) => {
-            let sectionTop = section.offsetTop - 200;
+            let sectionTop = section.offsetTop ;
             let sectionHeight = section.offsetHeight;
 
             if (scrollpos >= sectionTop && scrollpos < sectionTop + sectionHeight) {
