3f3e57106a7b4bcd39bf5b37e20629d8e346d875
abu0127 <sr20010127@gmail.com>
2025-03-27 20:31:42 +0300

    a

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const home_foto = document.querySelector('.home-foto');
 const greeting = document.querySelector('.greeting');
 const about = document.querySelector('.about');
 const names = document.querySelector('.name-con');
+const sections = document.querySelectorAll('.section')
 
 function main() {
     //    home photo animation
@@ -22,5 +23,36 @@ function main() {
     setTimeout(() => {
         names.classList.add('txt_animation')
     }, 1800)
+
+
+
+    window.addEventListener("scroll", function () {
+        let scrollpos = window.scrollY;
+
+        sections.forEach((section, index) => {
+            let sectionTop = section.offsetTop - 400;
+            let sectionHeight = section.offsetHeight;
+
+            if (scrollpos >= sectionTop && scrollpos < sectionTop + sectionHeight) {
+
+                
+                if (index === 1) {
+
+                    section.style.background = 'red'
+                }
+
+            }
+        })
+
+        
+    });
+
+
+
+
+
+
+
+
 
 }
